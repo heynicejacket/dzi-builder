7b6c98ba993b1a9ab6f30a54a0baec9f2e8ca5ca
SPACELAB\heynicejacket <matthew@nicejacket.cc>
2020-09-24 01:39:27 -0700

    update for transparent layer options with vips

--- dzi_builder/core/illustrator.py
+++ dzi_builder/core/illustrator.py
@@ -90,18 +90,7 @@ def create_artboards(app, doc, ai_path, width, height=0.0, transparent_png=True,
 
 def ai_to_svg(app, layer_path, verbose=False):
     """
-    Given a path to a folder, converts all files with the .ai extension into SVG files,
-    and returns a list of the file name groups. For example, a folder which contains:
-
-        base.ai             grid.ai
-        base-01.ai          grid-01.ai
-        base-02.ai          grid-02.ai
-        base-03.ai          grid-03.ai
-
-    ...returns a list:
-
-        ['base', 'grid']
-
+    Given a path to a folder, converts all files with the .ai extension into SVG files.
     :param app:             COM obj, required   reference to Illustrator (type win32com.client.CDispatch)
     :param layer_path:      str, required       folder and file path, e.g. 'C:\\path\\to\\file.ai'
     :param verbose:         bool, optional      if True, prints out details of task
@@ -111,8 +100,6 @@ def ai_to_svg(app, layer_path, verbose=False):
     output_list = [f for f in os.listdir(layer_path) if os.path.isfile(os.path.join(layer_path, f))]
     rem_list_pre = [l for l in output_list if l.find('-') == -1]
 
-    layer_name_list = [f.strip('.ai') for f in rem_list_pre]
-
     rem_list = prefix_path_to_list(layer_path, rem_list_pre)
     tile_list_pre = prefix_path_to_list(layer_path, output_list)
     tile_list = [f for f in tile_list_pre if f not in rem_list]
@@ -121,7 +108,7 @@ def ai_to_svg(app, layer_path, verbose=False):
 
     print('Converting *.ai to *.svg...') if verbose else None
     for t in tile_list:
-        print(t) if verbose else None
+        print('...{}'.format(t)) if verbose else None
         ai_tile = app.Open(t)
 
         js_convert_to_svg(app, layer_path.replace('\\', '/'))
@@ -130,8 +117,6 @@ def ai_to_svg(app, layer_path, verbose=False):
         ai_tile.Close()
         os.remove(t)
         time.sleep(2)                                           # keeps illustrator from overloading memory
-
-    return layer_name_list
 
 
 def get_layer_list(layer_path, verbose=False):
@@ -173,25 +158,32 @@ def create_folder(path, new_folder):
     return new_path
 
 
-def generate_tiles(file):
+def generate_tiles(file, tile_width, transparency):
     """
     Generate SVG or PNG tiles from an illustrator file with multiple layers and multiple artboards.
+
+    # transparent layer implementation:
+    # https://embers.nicejacket.cc/blog/2018/06/16/transparent-layers-for-openseadragon-with-libvips/
+
+    # non-transparent layer implementation:
+    # https://embers.nicejacket.cc/blog/2018/06/14/making-large-maps-with-openseadragon/
+
     :param file:            str, required       folder and file path, e.g. 'C:\path\to\file.ai'
+    :param tile_width:      float, required     width of output tiles
+    :param transparency:    bool, required      if True, tiles are generated with transparency in negative space
     :return:                list                list of layer names
     """
     tile_path = create_folder(file, 'layers')
 
     app = get_illustrator()
     doc = open_illustrator_file(app, file)
-    create_artboards(app, doc, tile_path, 300.0)
-
-    # non-transparent layer implementation:
-    # https://embers.nicejacket.cc/blog/2018/06/14/making-large-maps-with-openseadragon/
-    # layer_name_list = ai_to_svg(app, tile_path)
-
-    # transparent layer implementation:
-    # https://embers.nicejacket.cc/blog/2018/06/16/transparent-layers-for-openseadragon-with-libvips/
+    create_artboards(app=app, doc=doc, ai_path=tile_path, width=tile_width, transparent_png=transparency)
     layer_name_list = get_layer_list(tile_path)
+
+    if not transparency:
+        ai_to_svg(app, tile_path, verbose=True)
+
+    print(layer_name_list)
 
     app.Quit()
 
