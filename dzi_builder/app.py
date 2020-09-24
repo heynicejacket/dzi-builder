22d6e7a82bdbf35dbe726a85beb11ccad0b60299
SPACELAB\heynicejacket <matthew@nicejacket.cc>
2020-09-24 01:40:12 -0700

    update for transparent layer options with vips

--- dzi_builder/app.py
+++ dzi_builder/app.py
@@ -1,3 +1,5 @@
+import os
+
 from dzi_builder.core.illustrator import (
     generate_tiles
 )
@@ -12,10 +14,16 @@ from dzi_builder.core.image_magick import (
 
 src_file = 'C:\\localtemp\\demo.ai'
 layer_path = 'C:\\localtemp\\layers\\'
+transparency = True
+# transparency = False
 
-layer_list = generate_tiles(src_file)
+layer_list = generate_tiles(src_file, 300.0, transparency)
+if transparency:
+    pass
+else:
+    # these functions are for a basic, non-transparent layer implementation;
+    # see: https://embers.nicejacket.cc/blog/2018/06/14/making-large-maps-with-openseadragon/
+    convert_tiles(layer_path, 3000, verbose=True)
+    [os.remove(layer_path + f) for f in os.listdir(layer_path) if f.endswith('.svg')]
+    combine_tiles(layer_path, layer_list, 3000, 3, verbose=True)
 
-# these functions are for a basic, non-transparent layer implementation;
-# see: https://embers.nicejacket.cc/blog/2018/06/14/making-large-maps-with-openseadragon/
-# convert_tiles(layer_path, 3000, verbose=True)
-# combine_tiles(layer_path, layer_list, 3000, 3, verbose=True)
