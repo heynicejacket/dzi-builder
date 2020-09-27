9287063b80ef439008dd911c426ccb7d937aea86
SPACELAB\heynicejacket <matthew@nicejacket.cc>
2020-09-27 02:20:11 -0700

    made test script to dzi_builder function

--- dzi_builder/app.py
+++ dzi_builder/app.py
@@ -5,7 +5,8 @@ from dzi_builder.core.illustrator import (
 )
 
 from dzi_builder.core.vips import (
-    combine_transparent_layer
+    combine_transparent_layer,
+    make_image_pyramid
 )
 
 from dzi_builder.core.image_magick import (
@@ -13,18 +14,45 @@ from dzi_builder.core.image_magick import (
     combine_tiles
 )
 
+from dzi_builder.html.openseadragon_html import (
+    make_openseadragon_html,
+    make_openseadragon_css
+)
 
-exe_path = 'C:\\Program Files\\vips-dev-8.6\\bin\\'
-src_file = 'C:\\localtemp\\demo.ai'
-layer_path = 'C:\\localtemp\\layers\\'
-transparency = True
-# transparency = False
 
-layer_list = generate_tiles(src_file, 300.0, transparency)
-if transparency:
-    combine_transparent_layer(layer_path, 3, 3, 3000, 3000, exe_path, verbose=True)
-else:
-    # non-transparent layers; see: https://embers.nicejacket.cc/blog/2018/06/14/making-large-maps-with-openseadragon/
-    convert_tiles(layer_path, 3000, verbose=True)
-    [os.remove(layer_path + f) for f in os.listdir(layer_path) if f.endswith('.svg')]
-    combine_tiles(layer_path, layer_list, 3000, 3, verbose=True)
+def dzi_builder(ai_file, layers_path, vips_path, offset_right, offset_down=0, transparency=True, verbose=False):
+    """
+
+    :param ai_file:
+    :param layers_path:
+    :param vips_path:
+    :param offset_right:
+    :param offset_down:
+    :param transparency:
+    :param verbose:
+    :return:
+    """
+    offset_right_f = float(offset_right / 10)
+    offset_down_rect = offset_right if offset_down == 0 else offset_down
+
+    layer_list = generate_tiles(ai_file, offset_right_f, transparency)
+    if transparency:
+        png_layer_output = combine_transparent_layer(layers_path, 3, 3, offset_right, offset_down_rect, vips_path, verbose)
+    else:
+        convert_tiles(layers_path, offset_right, verbose=verbose)
+        [os.remove(layers_path + f) for f in os.listdir(layers_path) if f.endswith('.svg')]
+        combine_tiles(layers_path, layer_list, offset_down_rect, 3, verbose=verbose)
+        png_layer_output = [layer + '.png' for layer in layer_list]
+
+    make_image_pyramid(layers_path, png_layer_output, '', vips_path, verbose=verbose)
+
+
+dzi_builder(
+    ai_file='C:\\localtemp\\demo.ai',
+    layers_path='C:\\localtemp\\layers\\',
+    vips_path='C:\\Program Files\\vips-dev-8.10\\bin\\',
+    offset_right=3000,
+    offset_down=3000,
+    transparency=True,
+    verbose=False
+)
