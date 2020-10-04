97f53b6e1f0325df5e4e500de979128f511dee6c
SPACELAB\heynicejacket <matthew@nicejacket.cc>
2020-10-04 00:52:06 -0700

    added incomplete tile matrix functionality

--- dzi_builder/app.py
+++ dzi_builder/app.py
@@ -5,7 +5,8 @@ from dzi_builder.core.illustrator import (
 )
 
 from dzi_builder.core.toolkit import (
-    create_folder_structure
+    create_folder_structure,
+    get_file_list
 )
 
 from dzi_builder.core.vips import (
@@ -18,12 +19,20 @@ from dzi_builder.core.image_magick import (
     combine_tiles
 )
 
+from dzi_builder.core.tile_filler import (
+    build_matrix,
+    create_matrix,
+    get_filler_tiles,
+    restructure_layer_matrix
+)
+
 from dzi_builder.html.openseadragon_html import (
     make_site
 )
 
 
-def dzi_builder(ai_file, vips_path, col, row, offset_right, offset_down=0, transparency=True, verbose=False):
+def dzi_builder(ai_path, vips_path, col, row, offset_right,
+                offset_down=0, transparency=True, incomplete=False, verbose=False):
     """
     Given an illustrator file, creates a Deep Zoom Image for each top-level layer of the Illustrator file,
     and generates an html and css file to resolve a basic example, when adding the requisite openseadragon
@@ -56,38 +65,57 @@ def dzi_builder(ai_file, vips_path, col, row, offset_right, offset_down=0, trans
 
     See an example output here: https://embers.nicejacket.cc/viewer.html
 
-    :param ai_file:         str, required       path to Illustrator file, e.g. 'C:\\path\\to\\file.ai'
+    :param ai_path:         str, required       path to Illustrator file, e.g. 'C:\\path\\to\\file.ai'
     :param vips_path:       str, required       path to vips.exe, e.g. 'C:\\Program Files\\vips\\bin\\vips.exe'
     :param col:             int, required       count of artboard columns in Illustrator file (starting at 1)
     :param row:             int, required       count of artboard rows in Illustrator file (starting at 1)
     :param offset_right:    int, required       width of artboard tile
     :param offset_down:     int, optional       height of artboard tile
     :param transparency:    bool, optional      if True, runs subsequent functions through libvips, not ImageMagick
+    :param incomplete:      bool, optional      if True, requests user input to fill in missing tiles
     :param verbose:         bool, optional      if True, prints out details of task
     :return:                None
     """
     offset_right_f = float(offset_right / 10)
     offset_down_rect = offset_right if offset_down == 0 else offset_down
 
-    layer_path, html_path, dzi_path, osd_path = create_folder_structure(ai_file)
+    layer_path, html_path, dzi_path, osd_path = create_folder_structure(ai_path)
 
-    layer_list = generate_tiles(ai_file, offset_right_f, transparency)
+    layer_names = generate_tiles(ai_path, offset_right_f, transparency)
+
+    if incomplete:
+        tile_list = get_file_list(layer_path)
+        create_matrix(col, row)
+        filler_tile_ct, duplicates = get_filler_tiles()
+        layers_list, filler_list = build_matrix(tile_list, filler_tile_ct, duplicates, col, row)
+        restructure_layer_matrix(layer_path, layers_list, layer_names, filler_list)
+
     if transparency:
         combine_transparent_layer(layer_path, col, row, offset_right, offset_down_rect, vips_path, verbose)
     else:
         convert_tiles(layer_path, offset_right, verbose=verbose)
         [os.remove(layer_path + f) for f in os.listdir(layer_path) if f.endswith('.svg')]
-        combine_tiles(layer_path, layer_list, offset_down_rect, 3, verbose=verbose)
+        combine_tiles(layer_path, layer_names, offset_down_rect, 3, verbose=verbose)
 
-    make_image_pyramid(layer_path, layer_list, vips_path, verbose=verbose)
-    make_site(layer_path, layer_list)
+    make_image_pyramid(layer_path, layer_names, vips_path, verbose=verbose)
+    make_site(layer_path, layer_names)
 
 
+# basic implementation
 dzi_builder(
-    ai_file='C:\\localtemp\\demo-basic.ai',
+    ai_path='C:\\localtemp\\demo-simple.ai',
     vips_path='C:\\Program Files\\vips-dev-8.10\\bin\\',
     col=3,
     row=3,
-    offset_right=3000,
-    verbose=False
+    offset_right=3000
 )
+
+# # incomplete implementation
+# dzi_builder(
+#     ai_path='C:\\localtemp\\demo-incomplete.ai',
+#     vips_path='C:\\Program Files\\vips-dev-8.10\\bin\\',
+#     col=4,
+#     row=4,
+#     offset_right=3000,
+#     incomplete=True
+# )
